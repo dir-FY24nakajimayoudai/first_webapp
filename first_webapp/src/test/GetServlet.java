ca806399343b5ba23f86117a298a567be383590e
dir-FY24nakajimayoudai <youdai.nakajima.a478@dirbato.co.jp>
2024-04-18 14:44:58 +0900

    lesson16を終了しました

--- first_webapp/src/test/GetServlet.java
+++ first_webapp/src/test/GetServlet.java
@@ -29,7 +29,6 @@ public class GetServlet extends HttpServlet {
 	 */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
         String q = request.getParameter("q");
         request.setAttribute("q", q);
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/get_result.jsp");
--- first_webapp/src/test/SetAppScopeServlet.java
+++ first_webapp/src/test/SetAppScopeServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class SetAppScopeServlet
- */s
+ */
 @WebServlet("/SetAppScopeServlet")
 public class SetAppScopeServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
